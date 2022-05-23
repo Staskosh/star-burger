f8703d9f0a1a818363b787c302eedc38efb3e9c0
staskosh <staskoshenkov@rambler.ru>
2022-05-23 15:27:28 +0300

    Initial commit

---
+++ foodcartapp/views.py
@@ -0,0 +1,82 @@
+import json
+
+from django.http import JsonResponse
+from django.templatetags.static import static
+
+from star_burger import settings
+from .models import Product, Order, OrderItem
+
+
+def banners_list_api(request):
+    # FIXME move data to db?
+    return JsonResponse([
+        {
+            'title': 'Burger',
+            'src': static('burger.jpg'),
+            'text': 'Tasty Burger at your door step',
+        },
+        {
+            'title': 'Spices',
+            'src': static('food.jpg'),
+            'text': 'All Cuisines',
+        },
+        {
+            'title': 'New York',
+            'src': static('tasty.jpg'),
+            'text': 'Food is incomplete without a tasty dessert',
+        }
+    ], safe=False, json_dumps_params={
+        'ensure_ascii': False,
+        'indent': 4,
+    })
+
+
+def product_list_api(request):
+    products = Product.objects.select_related('category').available()
+
+    dumped_products = []
+    for product in products:
+        dumped_product = {
+            'id': product.id,
+            'name': product.name,
+            'price': product.price,
+            'special_status': product.special_status,
+            'description': product.description,
+            'category': {
+                'id': product.category.id,
+                'name': product.category.name,
+            } if product.category else None,
+            'image': product.image.url,
+            'restaurant': {
+                'id': product.id,
+                'name': product.name,
+            }
+        }
+        dumped_products.append(dumped_product)
+    return JsonResponse(dumped_products, safe=False, json_dumps_params={
+        'ensure_ascii': False,
+        'indent': 4,
+    })
+
+
+def register_order(request):
+    decoded_order = json.loads(request.body.decode())
+    print(decoded_order)
+    order = Order.objects.create(
+        firstname=decoded_order['firstname'],
+        lastname=decoded_order['lastname'],
+        phonenumber=decoded_order['phonenumber'],
+        address=decoded_order['address']
+    )
+    print(order)
+    products = decoded_order['products']
+    for product in products:
+        print(product['product'])
+        order_item = OrderItem.objects.create(
+            order=order,
+            product=Product.objects.get(id=product['product']),
+            quantity=product['quantity']
+        )
+        print(order, order_item)
+    # TODO это лишь заглушка
+    return JsonResponse({})
