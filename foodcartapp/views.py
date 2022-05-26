706010253c337b263e23ab7951736b5813e27c91
staskosh <staskoshenkov@rambler.ru>
2022-05-26 09:32:25 +0300

    Extend data validation for def register_order.

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,10 +1,11 @@
+import phonenumbers
 from django.http import JsonResponse
 from django.templatetags.static import static
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import Product, Order, OrderItem
+from .models import Order, OrderItem, Product
 
 
 def banners_list_api(request):
@@ -61,27 +62,52 @@ def product_list_api(request):
 
 @api_view(['POST'])
 def register_order(request):
-    if 'products' in request.data:
-        products = request.data['products']
-        if isinstance(products, list) and products != []:
-            order = Order.objects.create(
-                firstname=request.data['firstname'],
-                lastname=request.data['lastname'],
-                phonenumber=request.data['phonenumber'],
-                address=request.data['address']
-            )
-            for product in products:
-                OrderItem.objects.create(
-                    order=order,
-                    product=Product.objects.get(id=product['product']),
-                    quantity=product['quantity']
-                )
+    fields = ('products', 'firstname', 'lastname', 'phonenumber', 'address')
+    incorrect_fields = []
+    for field in fields:
+        if field not in request.data or request.data[field] is None or request.data[field] == []:
+            incorrect_fields.append(field)
 
-            return Response({}, status=status.HTTP_200_OK)
-        else:
-            content = {'error': 'products key is not presented or not list'}
-            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    if incorrect_fields:
+        content = {'error': f'{", ".join(incorrect_fields)} is/are not specified or not str'}
 
-    else:
-        content = {'error': 'products key is not presented or not list'}
         return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        products = request.data['products']
+        firstname = request.data['firstname']
+        lastname = request.data['lastname']
+        phonenumber = request.data['phonenumber']
+        address = request.data['address']
+
+        if isinstance(products, list) and products != []:
+            for product in products:
+                if not product['product'] <= Product.objects.last().id:
+                    content = {'error': f'products: not allowed the key {product["product"]}'}
+
+                    return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+            parsed_phonenumber = phonenumbers.parse(phonenumber, 'RU')
+            valid_phonenumber = phonenumbers.is_valid_number(parsed_phonenumber)
+            if phonenumber == '' or not valid_phonenumber:
+                content = {'error': f'phonenumber {phonenumber} is not valid'}
+
+                return Response(content, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                order = Order.objects.create(
+                    firstname=firstname,
+                    lastname=lastname,
+                    phonenumber=phonenumber,
+                    address=address
+                )
+                for product in products:
+                    OrderItem.objects.create(
+                        order=order,
+                        product=Product.objects.get(id=product['product']),
+                        quantity=product['quantity']
+                    )
+
+                return Response({}, status=status.HTTP_200_OK)
+        else:
+            content = {'error': 'products are not presented or not a list'}
+
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
