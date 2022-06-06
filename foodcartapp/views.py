1c50ae093c6f1ff6168fe257227b1031a62d20a8
staskosh <staskoshenkov@rambler.ru>
2022-06-06 10:12:20 +0300

    Change the name of item price variable .

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -77,13 +77,13 @@ def register_order(request):
     order_items = []
     for product in serializer.validated_data['products']:
         product_price = product['product'].price
-        price = Decimal(product_price * product['quantity'])
+        item_price = Decimal(product_price * product['quantity'])
         order_items.append(
             OrderItem(
                 order=order,
                 product=product['product'],
                 quantity=product['quantity'],
-                price=price
+                price=item_price
             )
         )
     OrderItem.objects.bulk_create(order_items)
