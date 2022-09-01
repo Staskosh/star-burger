4b676352b372b5e4d82ddb5696cd7a95f74f1b78
staskosh <staskoshenkov@rambler.ru>
2022-09-01 14:11:58 +0300

    Fix the deploy script

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -10,5 +10,4 @@ systemctl reload nginx
 REVISION=$(git rev-parse --short HEAD)
 ROLLBAR_POST_SERVER_TOKEN=$(bash -c 'set -a; source .env; printenv ROLLBAR_POST_SERVER_TOKEN')
 curl -H "X-Rollbar-Access-Token: $ROLLBAR_POST_SERVER_TOKEN" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "deployed", "revision": "'"$REVISION"'", "rollbar_name": "stx", "local_username": "root", "comment": "Starburger deployment", "status": "succeeded"}'
-
 echo 'Deploy successfully done.'
